1b8c7d6434772d9eb279588a0b26635f8918eeb7
Corey Black <coreyblack@hotmail.co.uk>
2025-03-27 20:45:12 +0000

    Testing

--- project/obj/Debug/net8.0/project.AssemblyInfo.cs
+++ project/obj/Debug/net8.0/project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd3a873ff1501405090208a873ccecb874986c27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9173627de5742617ebf8a447535ccd5ddeb95bb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
